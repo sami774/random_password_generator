716497a943d9927ca4f625445ebeb8dc6e3c24c4
sami774 <152988154+sami774@users.noreply.github.com>
2024-09-22 22:15:23 +0530

    random password generator react app version 1.0

---
+++ Password-generator/src/App.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState, useCallback, useRef } from "react";
+
+const App = () => {
+  const [passlength, setPasslength] = useState(8);
+  const [addnumber, setAddnumber] = useState(false);
+  const [addsymbol, setAddsymbol] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef(null);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let number = "0123456789";
+    let symbol = "!@#$%^&*-_+=[]{}~`";
+    if (addnumber) string += number;
+    if (addsymbol) string += symbol;
+    for (let i = 1; i <= passlength; i++) {
+      let Random = Math.floor(Math.random() * string.length);
+      pass += string.charAt(Random);
+    }
+    setPassword(pass);
+  }, [addnumber, addsymbol, passlength]); // Dependencies for useCallback
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [passwordGenerator]); // Depend on the memoized function
+
+  return (
+    <div className="main">
+      <h1>Random Password Generator</h1>
+      <div className="container">
+        <input className="display" type="text" readOnly value={password} ref={passwordRef} />
+        <button onClick={copyPasswordToClipboard}>
+          Copy
+        </button>
+        <div className="button-container">
+          <div className="slider">
+            <input
+              type="range"
+              min={1}
+              max={100}
+              value={passlength}
+              onChange={(e) => setPasslength(Number(e.target.value))}
+            />
+            <label>length: {passlength}</label>
+          </div>
+          <div className="number">
+            <input
+              type="checkbox"
+              checked={addnumber}
+              onChange={() => setAddnumber((prev) => !prev)}
+            />
+            <label>add number</label>
+          </div>
+          <div className="symbol">
+            <input
+              type="checkbox"
+              checked={addsymbol}
+              onChange={() => setAddsymbol((prev) => !prev)}
+            />
+            <label>add special symbol</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
